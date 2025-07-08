3257236bca484d865565651cb0566cfe8d9bde70
Francesco Citterio <francescocitterio99@gmail.com>
2025-07-08 22:34:37 +0200

    correct path in sbatch multi gpu example again4

--- multi_gpu_torch_examples/ddp-tutorial-series/slurm/sbatch_run.sh
+++ multi_gpu_torch_examples/ddp-tutorial-series/slurm/sbatch_run.sh
@@ -8,6 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --constraint=gpu
 #SBATCH --ntasks-per-node=4
+#SBATCH --partition=gpudev
 
 #SBATCH --gres=gpu:a100:4
 #SBATCH --nvmps
@@ -19,18 +20,31 @@
 module purge
 module load anaconda/3/2023.03 pytorch/gpu-cuda-12.1/2.2.0
 
-nodes=( $( scontrol show hostnames $SLURM_JOB_NODELIST ) )
-nodes_array=($nodes)
-head_node=${nodes_array[0]}
-head_node_ip=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --ip-address)
+# nodes=( $( scontrol show hostnames $SLURM_JOB_NODELIST ) )
+# nodes_array=($nodes)
+# head_node=${nodes_array[0]}
+# head_node_ip=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --ip-address)
 
-echo Node IP: $head_node_ip
-export LOGLEVEL=INFO
+# echo Node IP: $head_node_ip
+# export LOGLEVEL=INFO
 
-srun torchrun \
---nnodes 4 \
---nproc_per_node 1 \
---rdzv_id $RANDOM \
---rdzv_backend c10d \
---rdzv_endpoint $head_node_ip:29500 \
-multinode_torchrun.py 50 10
+# srun torchrun \
+# --nnodes $SLURM_NNODES \
+# --nproc_per_node $SLURM_GPUS_ON_NODE
+# # --rdzv_id $RANDOM \
+# # --rdzv_backend c10d \
+# # --rdzv_endpoint $head_node_ip:29500 \
+# multinode_torchrun.py 50 10
+
+
+export MASTER_PORT=29500
+export MASTER_ADDR=$(hostname)
+export NCCL_DEBUG=INFO
+
+torchrun \
+  --nnodes $SLURM_NNODES \
+  --nproc_per_node $SLURM_GPUS_ON_NODE \
+  --rdzv_id $SLURM_JOB_ID \
+  --rdzv_backend c10d \
+  --rdzv_endpoint $MASTER_ADDR:$MASTER_PORT \
+  multinode_torchrun.py 50 10
